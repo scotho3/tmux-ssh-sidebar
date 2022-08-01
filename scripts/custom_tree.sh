a93bd5bb403e36876bd601e74b4059e5cd8fe171
Thomas Scott <tscott@digitalocean.com>
2022-08-01 15:36:32 -0400

    loop without =

--- scripts/custom_tree.sh
+++ scripts/custom_tree.sh
@@ -3,11 +3,7 @@
 # find . -path '*/.git*' -prune -o -print |
 # 	sed -e 's;[^/]*/;|___;g;s;___|; |;g'
 
-<<<<<<< HEAD
 find_host() {
-=======
-find_host() = {
->>>>>>> 33bc4f3f665c07940b6d22874a9d81b973c7c6e8
 
 host=$(grep -e "^Host " ~/.ssh/network | awk '{print $2}' | grep -v "\*" | fzf)
 if [ -z $host ]
