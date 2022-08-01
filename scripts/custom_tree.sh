33bc4f3f665c07940b6d22874a9d81b973c7c6e8
Thomas Scott <tscott@digitalocean.com>
2022-08-01 15:34:38 -0400

    loop

--- scripts/custom_tree.sh
+++ scripts/custom_tree.sh
@@ -3,6 +3,8 @@
 # find . -path '*/.git*' -prune -o -print |
 # 	sed -e 's;[^/]*/;|___;g;s;___|; |;g'
 
+find_host() = {
+
 host=$(grep -e "^Host " ~/.ssh/network | awk '{print $2}' | grep -v "\*" | fzf)
 if [ -z $host ]
 then
@@ -11,3 +13,7 @@ else
 		echo "SSH session started, connecting to" $host
 		ssh $host
 fi
+
+}
+
+while :; do find_host; sleep 1; done
