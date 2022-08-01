b232842541dd7cfb22793114f97d7192d39b839c
Thomas Scott <tscott@digitalocean.com>
2022-08-01 15:25:18 -0400

    test

--- scripts/custom_tree.sh
+++ scripts/custom_tree.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-find . -path '*/.git*' -prune -o -print |
-	sed -e 's;[^/]*/;|___;g;s;___|; |;g'
+# find . -path '*/.git*' -prune -o -print |
+# 	sed -e 's;[^/]*/;|___;g;s;___|; |;g'
+
+host=$(grep -e "^Host " ~/.ssh/network | awk '{print $2}' | grep -v "\*" | fzf)
+if [ -z $host ]
+then
+		return 1
+else
+		echo "SSH session started, connecting to" $host
+		ssh $host
+fi
